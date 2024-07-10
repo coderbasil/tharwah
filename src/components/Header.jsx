fbc6d10a60c87e97b52f19ba3f07ae389411a78d
Basil <bsuli1424@gmail.com>
2024-07-10 05:33:34 +0300

    v0.03

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,27 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <div className="flex-container headerDiv">
-        <div className="flex-item logoDiv">
-          <img
-            className="logo"
-            src="https://cdn.salla.sa/NOYPq/iFHDPZdriZ0mKQBVDwQBXoAB0RjxHnstx0DC1NNz.jpg"
-            alt="logo"
-          />
-        </div>
-        <div className="flex-item logoName">
-          <span style={{ fontSize: 30 }}></span>
-        </div>
+      <div className="flex-item">
+        <img
+          className="logo"
+          src="https://cdn.salla.sa/NOYPq/iFHDPZdriZ0mKQBVDwQBXoAB0RjxHnstx0DC1NNz.jpg"
+          alt="logo"
+        />
+      </div>
 
-        <div className="flex-item divDetector">
-          <a href="">عننا</a>
-        </div>
+      <div className="nav-items">
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">About us</a>
+        </li>
+        <li>
+          <a href="#">About us</a>
+        </li>
+        <li>
+          <a href="#">About us</a>
+        </li>
       </div>
     </header>
   );
