595c1de702c47ae674b4f44cdb69ebdf3c63df69
Basil <bsuli1424@gmail.com>
2024-07-24 12:58:19 +0300

    nevermind cuz i don't know how to use Git

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,23 +6,23 @@ function Header() {
   return (
     <header className="sticky">
       <div className="logo">
-        <a href="/tharwah">
+        <a href="/">
           <img src={logo} alt="logo" />
         </a>
       </div>
 
       <div className="nav-items">
         <li>
-          <a href="tharwah/about-us">عن ثروة</a>
+          <a href="/about-us">عن ثروة</a>
         </li>
         <li>
           <a href="https://tharwah2023.com/">متجرنا</a>
         </li>
         <li>
-          <a href="tharwah/ped-blogs">المنشورات البيطرية</a>
+          <a href="/ped-blogs">المنشورات البيطرية</a>
         </li>
         <li>
-          <a href="tharwah/excel-sheet">جداول المتابعه</a>
+          <a href="/excel-sheet">جداول المتابعه</a>
         </li>
       </div>
       <div>
@@ -32,9 +32,9 @@ function Header() {
               <i class="fa fa-caret-down arrow"></i>
             </button>
             <div class="dropdown-content">
-              <a href="tharwah/ped-blogs">المنشورات البيطرية</a>
-              <a href="tharwah/excel-sheet">جداول المتابعه</a>
-              <a href="tharwah/about-us">عن ثروة</a>
+              <a href="/ped-blogs">المنشورات البيطرية</a>
+              <a href="/excel-sheet">جداول المتابعه</a>
+              <a href="/about-us">عن ثروة</a>
               <a href="https://tharwah2023.com/">متجرنا</a>
             </div>
           </div>
