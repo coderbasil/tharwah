c8b4745c10a596a6fd3a1ced82fa4a4f298ef23b
Basil <bsuli1424@gmail.com>
2024-08-30 22:16:23 +0300

    added express to fix the react browser router issue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import express from "express"
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const app = express();
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 root.render(
   <React.StrictMode>
     <App />
