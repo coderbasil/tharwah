3cfc93a4a22ebff0eb103417b959146143ee87a6
Basil <bsuli1424@gmail.com>
2024-08-30 22:40:55 +0300

    trying to launch

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import express from "express"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', '../public/index.html'));
 });
 
 root.render(
