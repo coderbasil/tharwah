a464eedecef17056f79832edee61c4bcf4b38acc
Basil <bsuli1424@gmail.com>
2024-09-06 22:35:47 +0300

    added css to the src dir

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import Footer from "./components/Footer";
 import Pedblogs from "./pages/Pedblogs";
 import Pedblog from "./pages/Pedblog";
 import Sheets from "./pages/Sheets";
+import "./css/styles.css"
 
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/tharwah">
       <div>
         <Header></Header>
         <div>
             <Routes>
               <Route index element={<Home/>}/>
-              <Route path="/ped-blogs" element={<Pedblogs/>}/>
-              <Route path="/ped-blogs/:blogtitle" 
+              <Route path="ped-blogs" element={<Pedblogs/>}/>
+              <Route path="ped-blogs/:blogtitle" 
               loader={({params})=>{console.log(params.blogtitle)}} 
               action={({ params }) => {}}
               element ={<Pedblog/>}/>
-              <Route path="/excel-sheet" element={<Sheets/>}/>
+              <Route path="excel-sheet" element={<Sheets/>}/>
+              <Route path="*" element={<Home/>}/>
             </Routes>
         </div>
         <Footer></Footer>
