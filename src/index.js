6a18217f98f01154cb6a46771cf6fdd221d5cb7d
Basil <bsuli1424@gmail.com>
2024-09-06 21:28:32 +0300

    added .htaccess file hope the site works now

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import express from "express"
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', '../public/index.html'));
-});
+
 
 root.render(
   <React.StrictMode>
